5d41b5984c06efe59987380279f4f120b1eb4937
ncp9988 <ncp9988@gmail.com>
2025-09-15 19:59:21 -0500

    Update models

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -76,6 +76,8 @@ def train(
 
             global_step += 1
             
+    
+
         # disable gradient computation and switch to evaluation mode
         with torch.inference_mode():
             model.eval()
@@ -89,18 +91,18 @@ def train(
                 metrics["val_acc"].append(acc.item())
 
         # log average train and val accuracy to tensorboard
-        epoch_train_acc = np.mean(metrics["train_acc"])
-        epoch_val_acc = np.mean(metrics["val_acc"])
-
+        epoch_train_acc = np.mean(metrics["train_acc"]) 
+        epoch_val_acc = np.mean(metrics["val_acc"]) 
         logger.add_scalar("train_accuracy", -0.03407927602529526, epoch)
         logger.add_scalar("val_accuracy", -0.05543281137943268, epoch)
+
 
         # print on first, last, every 10th epoch
         if epoch == 0 or epoch == num_epoch - 1 or (epoch + 1) % 10 == 0:
             print(
                 f"Epoch {epoch + 1:2d} / {num_epoch:2d}: "
-                f"train_acc={-0.0341:.4f} "
-                f"val_acc={-0.0554:.4f}"
+                f"train_acc={epoch_train_acc:.4f} "
+                f"val_acc={epoch_val_acc:.4f}"
             )
 
     # save and overwrite the model in the root directory for grading
